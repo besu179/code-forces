676df45a9db1c29487e01e6ec6a3e1b129715df2
besu178 <besuwinner@gmail.com>
2026-02-07 21:34:46 +0300

    .

--- C_Check_valid_numbers.py
+++ C_Check_valid_numbers.py
@@ -2,4 +2,11 @@ test_num = int(input())
 for i in range(test_num):
     n, m, p, q = map(int, input().split())
     
-    if n < 
+    
+    if n % p == 0:
+        if m == (n // p) * q:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("YES")
