2dec96e31ea7a1c3dd55ca02c527823b7955f830
besu178 <besuwinner@gmail.com>
2026-03-06 20:41:19 +0300

    ,

--- B_Ehab_Is_an_Odd_Person.py
+++ B_Ehab_Is_an_Odd_Person.py
@@ -1,3 +1,14 @@
 n = int(input())
 a = list(map(int, input().split()))
 
+even = False
+odd = False
+
+for num in a:
+    if num % 2 == 0:
+        even = True
+    else:
+        odd = True
+if even and odd:
+    a.sort()
+print(" ".join(map(str, a)))
